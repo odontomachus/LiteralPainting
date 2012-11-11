b8d51fc3717d35be73d0384356f6777e5269645a
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2012-11-11 15:24:04 -0500

    Code cleanup and commenting

--- literalpainting/__init__.py
+++ literalpainting/__init__.py
@@ -1,58 +1,16 @@
 # -*- coding: utf-8 -*-
 import sys
 import re
-import gettext
-import random
 
 from nltk.data import load as data_load
 from nltk.grammar import (Production, FeatureGrammar, FeatStructNonterminal)
 from nltk.parse.earleychart import FeatureEarleyChartParser
 from nltk.featstruct import FeatStructParser
+
 from bottle import jinja2_view as view
-from bottle import jinja2_template as template
-from bottle import route, post, get, run, redirect, request, Bottle
-from bottle import Jinja2Template
+from bottle import route, post, get, run, request
 from bottle import static_file
 
-class Integer(object):
-    """ A class that always returns true if it is compared to a number. """
-    def __hash__(self):
-        return hash(getattr(self, 'value', 0))
-
-    def __eq__(self, other):
-        try:
-            assert int(other) > 1
-            self.value = other
-            return True
-        except:
-            return False
-
-    def __ne__(self, other):
-        try:
-            assert int(other)
-            self.value = other
-            return False
-        except:
-            return True
-
-
-class IntDict(dict):
-    """ Dict which returns i for d[i] if i can be converted to an integer. """
-    def __getitem__(self, key):
-        try:
-            return int(key)
-        except:
-            return super(IntDict, self).__getitem__(key)
-
-    def get(self, key, value=None):
-        try:
-            int(key) 
-            return key
-        except:
-            return super(IntDict, self).get(key, value)
-
-# In order to accept all numbers, add a production to a terminal which
-# accepts "all" objects which can be converted to an integer (eg '100').
 grammar = data_load('file:commandParser.fcfg')
 productions = grammar.productions()
 
@@ -121,6 +79,7 @@ def parse():
     command = request.forms.get('command').strip(' .?!')
     tokens = command.split()
 
+    # Make a local copy of productions
     lproductions = list(productions)
 
     # find all integers
@@ -128,9 +87,10 @@ def parse():
     # Add a production for every integer
     lproductions.extend(map(num_production, ints))
 
+    # Make a local copy of the grammar with extra productions
     lgrammar = FeatureGrammar(grammar.start(), lproductions)
 
-    lgrammar._lexical_index = IntDict(lgrammar._lexical_index)
+    # Load grammar into a parser
     parser = FeatureEarleyChartParser(lgrammar, trace=0)
 
     try:
@@ -143,7 +103,7 @@ def parse():
             status = True
             data = {
                 'tree': trees[0],
-                # @TODO
+                # Eval semantics 
                 'actions': [eval(str(trees[0].node['SEM']))]
                 }
     except ValueError as e:
@@ -151,12 +111,11 @@ def parse():
     return {'status':status, 'errors':errors, 'data':data}
 
 
+
 @route('/static/<filepath:path>')
 def server_static(filepath):
     """ Serve static files (css/js/images). """
     return static_file(filepath, root='./static')
-
-
 
 if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True, reloader=True)
